af11f3e2ddb06187f9a50b05438e0cc95d64342d
Rianna Winston <riannawinston@macbook-air.myfiosgateway.com>
2023-07-18 19:15:52 -0400

    first commit

---
+++ BackEnd/Data/products.js
@@ -0,0 +1,110 @@
+products = [
+    {
+        "department": "Womens",
+        "product_title": "Elegant Silk Dress",
+        "description": "A luxurious silk dress that exudes elegance and sophistication.",
+        "price": 299.99,
+        "material": "100% Silk",
+        "care_instructions": "Dry clean only",
+        "product_no": 758943
+    },
+    {
+        "department": "Womens",
+        "product_title": "Cashmere Sweater",
+        "description": "A soft and cozy cashmere sweater for ultimate comfort and style.",
+        "price": 249.99,
+        "material": "100% Cashmere",
+        "care_instructions": "Hand wash cold, lay flat to dry",
+        "product_no": 649327
+    },
+    {
+        "department": "Womens",
+        "product_title": "Leather Jacket",
+        "description": "A timeless leather jacket that adds a touch of edginess to any outfit.",
+        "price": 499.99,
+        "material": "Genuine Leather",
+        "care_instructions": "Professional leather clean only",
+        "product_no": 312598
+    },
+    {
+        "department": "Womens",
+        "product_title": "Tailored Trousers",
+        "description": "Classic tailored trousers with a flattering fit, perfect for formal occasions.",
+        "price": 179.99,
+        "material": "62% Polyester / 33% Viscose / 5% Elastane",
+        "care_instructions": "Machine wash cold, tumble dry low",
+        "product_no": 874621
+    },
+    {
+        "department": "Womens",
+        "product_title": "Silk Blouse",
+        "description": "A luxurious silk blouse that adds a touch of elegance to any ensemble.",
+        "price": 179.99,
+        "material": "100% Silk",
+        "care_instructions": "Dry clean only",
+        "product_no": 539247
+    },
+    {
+        "department": "Womens",
+        "product_title": "Designer Handbag",
+        "description": "A chic designer handbag crafted with exquisite attention to detail.",
+        "price": 699.99,
+        "material": "Genuine Leather",
+        "care_instructions": "Keep away from moisture, clean with a soft cloth",
+        "product_no": 186540
+    },
+    {
+        "department": "Mens",
+        "product_title": "Wool Suit",
+        "description": "A sophisticated wool suit for the modern gentleman.",
+        "price": 799.99,
+        "material": "100% Wool",
+        "care_instructions": "Dry clean only",
+        "product_no": 789012
+    },
+    {
+        "department": "Mens",
+        "product_title": "Leather Belt",
+        "description": "A stylish leather belt that complements any outfit.",
+        "price": 99.99,
+        "material": "Genuine Leather",
+        "care_instructions": "Wipe clean with a damp cloth",
+        "product_no": 925703
+    },
+    {
+        "department": "Mens",
+        "product_title": "Cotton Shirt",
+        "description": "A classic cotton shirt for a polished and refined look.",
+        "price": 129.99,
+        "material": "100% Cotton",
+        "care_instructions": "Machine wash cold, tumble dry low",
+        "product_no": 407162
+    },
+    {
+        "department": "Mens",
+        "product_title": "Suede Shoes",
+        "description": "Stylish suede shoes that offer both comfort and elegance.",
+        "price": 249.99,
+        "material": "Suede Leather",
+        "care_instructions": "Protect with suede cleaner, avoid water exposure",
+        "product_no": 683925
+    },
+    {
+        "department": "Mens",
+        "product_title": "Tailored Trousers",
+        "description": "Classic tailored trousers with a modern fit, perfect for formal occasions.",
+        "price": 199.99,
+        "material": "72% Wool / 25% Polyester / 3% Elastane",
+        "care_instructions": "Dry clean only",
+        "product_no": 124086
+    },
+    {
+        "department": "Mens",
+        "product_title": "Wool Sweater",
+        "description": "A warm and stylish wool sweater for a cozy yet sophisticated look.",
+        "price": 179.99,
+        "material": "100% Merino Wool",
+        "care_instructions": "Hand wash cold, lay flat to dry",
+        "product_no": 596431
+    }
+]
